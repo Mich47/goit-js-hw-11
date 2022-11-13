b20180a3a2e72c48c0fe3afa9ae14c859c9ed525
Dmytro Muzyka <47075277+Mich47@users.noreply.github.com>
2022-11-14 00:16:58 +0200

    add base code and styles

--- src/fetchCountries.js
+++
@@ -1,9 +0,0 @@
-export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
-    if (!response.ok) {
-      throw new Error('Oops, there is no country with that name');
-    }
-
-    return response.json();
-  });
-}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import * as debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
 
-class CountrysInfo {
+class Gallery {
   DEBOUNCE_DELAY = 300;
 
-  constructor({ input, countryList, countryInfo }) {
-    this.input = input;
-    this.countryList = countryList;
-    this.countryInfo = countryInfo;
+  constructor({ searchForm, gallery }) {
+    this.searchForm = searchForm;
+    this.gallery = gallery;
   }
 
   init() {
@@ -17,116 +15,36 @@ class CountrysInfo {
   }
 
   addListeners() {
-    this.input.addEventListener(
-      'input',
-      debounce(this.onInput.bind(this), this.DEBOUNCE_DELAY)
-    );
-    this.input.addEventListener('keydown', this.onInputEnter.bind(this));
-    this.countryList.addEventListener('click', this.onClick.bind(this));
+    console.log('this.searchForm ', this.searchForm);
+    console.log('this.gallery = gallery; ', this.gallery);
+    this.getImages();
   }
 
-  onInput(event) {
-    const inputValue = event.target.value.trim();
-    if (!inputValue) {
-      this.clearCountry(); //Clear
-      return;
-    }
-
-    this.getCountries(inputValue);
-  }
-
-  onInputEnter(event) {
-    if (event.code != 'Enter' && event.code != 'NumpadEnter') {
-      return;
-    }
-    this.onInput(event);
-  }
-
-  onClick(event) {
-    if (event.target.nodeName === 'UL') {
-      return;
-    }
-
-    if (event.target.nodeName === 'IMG') {
-      this.getCountries(event.target.parentNode.innerText);
-      return;
-    }
-
-    this.getCountries(event.target.innerText);
-  }
-
-  getCountries(inputValue) {
-    fetchCountries(inputValue)
-      .then(this.getCountriesData.bind(this))
+  getImages() {
+    this.fetchImages()
+      .then(console.log)
       .catch(error => {
-        this.clearCountry(); //Clear
         Notify.failure(error.message);
       });
   }
 
-  getCountriesData(data) {
-    // console.log('data ', data);
-    if (data.length > 10) {
-      this.clearCountry(); //Clear
-      Notify.info('Too many matches found. Please enter a more specific name.');
-      return null;
-    }
-    if (data.length === 1) {
-      this.addCountryList(data);
-      this.addCountryInfo(data);
-      return null;
-    }
+  fetchImages() {
+    return fetch(
+      `https://pixabay.com/api/?key=31303071-b4e5345642141d1af1d763c20&q=yellow+flowers&image_type=photo`
+    ).then(response => {
+      if (!response.ok) {
+        console.log('response ', response);
+        throw new Error('Oops, there is no country with that name');
+      }
 
-    this.clearCountry(); //Clear
-    this.addCountryList(data);
-  }
-
-  getCountryListMarkup(data) {
-    return data
-      .map(({ flags: { svg }, name: { official } }) => {
-        return `
-        <li class="country-item">
-          <img src="${svg}" width="60">
-          <div>
-          <p>${official}</p>
-          </div>
-        </li>`;
-      })
-      .join('');
-  }
-
-  getCountryInfoMarkup(data) {
-    return data
-      .map(({ name: { common }, capital, population, languages }) => {
-        return `
-        <div>
-          <p>Common Name: <span>${common}</span></p>
-          <p>Capital: <span>${capital}</span></p>
-          <p>Population: <span>${population}</span></p>
-          <p>Languages: <span>${Object.values(languages).join(', ')}</span></p>
-        </div>`;
-      })
-      .join('');
-  }
-
-  addCountryList(data) {
-    this.countryList.innerHTML = this.getCountryListMarkup(data);
-  }
-
-  addCountryInfo(data) {
-    this.countryInfo.innerHTML = this.getCountryInfoMarkup(data);
-  }
-
-  clearCountry() {
-    this.countryList.innerHTML = '';
-    this.countryInfo.innerHTML = '';
+      return response.json();
+    });
   }
 }
 
 const refs = {
-  input: document.querySelector('[id="search-box"]'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
+  searchForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
 };
 
-new CountrysInfo(refs).init();
+new Gallery(refs).init();
