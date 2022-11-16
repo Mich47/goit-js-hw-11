4d4ae9001206de48b5fce2bd4cb6847ae091cb55
Mich47 <mmitya889@gmail.com>
2022-11-16 17:14:50 +0200

    add some code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import * as debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
+const axios = require('axios').default;
 
 class Gallery {
   DEBOUNCE_DELAY = 300;
@@ -15,8 +16,8 @@ class Gallery {
   }
 
   addListeners() {
-    console.log('this.searchForm ', this.searchForm);
-    console.log('this.gallery = gallery; ', this.gallery);
+    // console.log('this.searchForm ', this.searchForm);
+    // console.log('this.gallery = gallery; ', this.gallery);
     this.getImages();
   }
 
@@ -26,14 +27,39 @@ class Gallery {
       .catch(error => {
         Notify.failure(error.message);
       });
+
+    this.fetchImagesA()
+      .then(console.log)
+      .catch(error => {
+        Notify.failure(error.message);
+      });
+  }
+
+  async fetchImagesA() {
+    try {
+      const response = await axios.get(
+        `https://pixabay.com/api/?key=31303071-b4e5345642141d1af1d763c20&q=yellow+flowers&image_type=photo`
+      );
+      console.log('response axios ->', response);
+
+      const data = await response.data;
+      console.log('data ->', data);
+
+      const dataArr = await data.hits;
+      console.log('dataArr ->', dataArr);
+
+      return dataArr;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   fetchImages() {
     return fetch(
       `https://pixabay.com/api/?key=31303071-b4e5345642141d1af1d763c20&q=yellow+flowers&image_type=photo`
     ).then(response => {
+      console.log('response ', response);
       if (!response.ok) {
-        console.log('response ', response);
         throw new Error('Oops, there is no country with that name');
       }
 
